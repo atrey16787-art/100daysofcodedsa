8f70bdc3adff47cc4ccdda416bcaea8f9d79f99c
Atrey Shukla <atrey.16787@gmail.com>
2026-04-30 02:40:01 +0530

    Add files via upload

---
+++ day78.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <limits.h>
+
+#define MAX 100
+
+int minKey(int key[], int mstSet[], int n) {
+    int min = INT_MAX, min_index;
+
+    for (int v = 1; v <= n; v++) {
+        if (!mstSet[v] && key[v] < min) {
+            min = key[v];
+            min_index = v;
+        }
+    }
+    return min_index;
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int graph[MAX][MAX];
+
+    // Initialize graph
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            graph[i][j] = 0;
+        }
+    }
+
+    // Input edges
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        scanf("%d %d %d", &u, &v, &w);
+        graph[u][v] = w;
+        graph[v][u] = w; // undirected
+    }
+
+    int key[MAX], mstSet[MAX];
+
+    for (int i = 1; i <= n; i++) {
+        key[i] = INT_MAX;
+        mstSet[i] = 0;
+    }
+
+    key[1] = 0;  // start from node 1
+    int totalWeight = 0;
+
+    for (int count = 1; count <= n; count++) {
+        int u = minKey(key, mstSet, n);
+        mstSet[u] = 1;
+
+        totalWeight += key[u];
+
+        for (int v = 1; v <= n; v++) {
+            if (graph[u][v] && !mstSet[v] && graph[u][v] < key[v]) {
+                key[v] = graph[u][v];
+            }
+        }
+    }
+
+    printf("%d\n", totalWeight);
+
+    return 0;
+}
